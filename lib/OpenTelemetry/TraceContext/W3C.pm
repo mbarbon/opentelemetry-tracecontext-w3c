a8fe2c1abc5912fcb9bdfd1a070c279db5abf8ec
Mattia Barbon <mattia@barbon.org>
2022-11-10 20:11:57 +0100

    Initial commit

---
+++ lib/OpenTelemetry/TraceContext/W3C.pm
@@ -0,0 +1,14 @@
+package OpenTelemetry::TraceContext::W3C;
+# ABSTRACT: W3C Trace Context implementation
+
+use strict;
+use warnings;
+use Exporter qw();
+
+# VERSION
+
+*import = \&Exporter::import;
+
+1;
+
+__END__
